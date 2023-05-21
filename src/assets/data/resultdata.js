5be38937f8ff9b30a3cd990c4aa89070a4a9b4ec
wbwhdrhadl <daeunrhadl@naver.com>
2023-05-21 12:48:43 +0900

    add result page

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,122 @@
+import Abisini from '../cat/Abisini.jpg';
+import Amarican from '../cat/Amarican.jpg';
+import Bangal from '../cat/Bangal.jpg';
+import Britsh from '../cat/Britsh.jpg';
+import Korea from '../cat/Korea.jpg';
+import Maincun from '../cat/Maincun.jpg';
+import Noriway from '../cat/Noriway.jpg';
+import Racdom from '../cat/Racdom.jpg';
+import Russian from '../cat/Russian.jpg';
+import Sam from '../cat/Sam.jpg';
+import Scotish from '../cat/Scotish.jpg';
+import Spin from '../cat/Spin.jpg';
+import Turkey from '../cat/Turkey.jpg';
+import acjo from '../cat/acjo.jpg';
+
+
+
+export const ResultData=[
+    {
+        id:1,
+        name:'메인쿤',
+        best:'ENFJ',
+        desc:'거인 신사라는 별명을 가졌어요',
+        image:`${Maincun}`
+
+    },
+    {
+        id:2,
+        name:'스핑크스',
+        best:'ISTP',
+        desc:'유일하게 털이 없는 고양이',
+        image:`${Spin}`
+
+    },
+    {
+        id:3,
+        name:'브리티쉬 숏헤어',
+        best:'ENTP',
+        desc:'이름과 같이 영국 신사와 같은 고양이',
+        image:`${Britsh}`
+
+    },
+    {
+        id:4,
+        name:'렉돔',
+        best:'ESFJ',
+        desc:'긴 털과 아주 예쁜 외모 소유',
+        image:`${Racdom}`
+
+    },
+    {
+        id:5,
+        name:'샴',
+        best:'ESFP',
+        desc:'대체로 온순하고 느긋한 성격',
+        image:`${Sam}`
+
+    },
+    {
+        id:6,
+        name:'뱅갈',
+        best:'INTP',
+        desc:'활동력이 매우 강함',
+        image:`${Bangal}`
+
+    },
+    {
+        id:7,
+        name:'터키쉬앙고라',
+        best:'INTJ',
+        desc:'우아한 장모 고양이',
+        image:`${Turkey}`
+
+    },
+    {
+        id:8,
+        name:'아비시니안',
+        best:'ISTJ',
+        desc:'매우 민첩 똑똑',
+        image:`${Abisini}`
+
+    },
+    {
+        id:9,
+        name:'노르웨이 숲',
+        best:'ESTP',
+        desc:'조용하고 사람을 좋아함',
+        image:`${Noriway}`
+    },
+    {
+        id:10,
+        name:'아메리칸 숏헤어',
+        best:'INFP',
+        desc:'주변에 호기심이 높음',
+        image:`${Amarican}`
+    },
+    {
+        id:11,
+        name:'코리안 숏헤어',
+        best:'ISFP',
+        desc:'많은 고양이가 섞인 종',
+        image:`${Korea}`
+    },
+
+    {
+        id:12,
+        name:'엑준틱',
+        best:'ISFJ',
+        desc:'낮을 가리지 않아요',
+        image:`${acjo}`
+    },
+
+    {
+        id:13,
+        name:'스코티쉬 폴드',
+        best:'ENFP',
+        desc:'접힌 귀가 굉장히 귀여움',
+        image:`${Scotish}`
+    }
+
+
+]
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,65 @@
 import React from 'react';
+//css-in-js
+import styled from 'styled-components';
+import PangImage from '../assets/cutecat.jpg';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import {ResultData} from '../assets/data/resultdata';
+
 
 const Result=()=>{
-    return(<div>결과화면입니다.</div>)
+    const navigate=useNavigate();
+    return(        
+    <Wrapper>
+        <Header>예비집사 판별기.</Header>
+        <Contents>
+            <Title>결과 보기</Title>
+            <LogoImage>
+                <img src={ResultData[0].image} className="rounded-circle" width={350} height={(350)}/>
+            </LogoImage>
+            <Desc>예비 집사님과 찰떡 궁합인 고양이는 {ResultData[0].name}입니다.</Desc>
+            <Button style={{fontFamily:"header"}}onClick={()=>navigate("/")}>
+                테스트 다시하기
+            </Button>
+        </Contents>
+    </Wrapper>)
 }
 
 export default Result;
+
+
+const Wrapper=styled.div`
+    height:100vh;
+    width:100%;    
+`
+const Header=styled.div`
+    font-size:40pt;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    font-family:"header";
+
+`
+const Contents=styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+`
+const Title=styled.div`
+    font-size:30pt;
+    margin-top:40px;
+    font-family:"header";
+
+`
+const LogoImage=styled.div`
+    margin-top:10px;
+
+`
+
+const Desc=styled.div`
+    font-size:20pt;
+    margin-top:20px;
+    font-family:"header";
+
+`
